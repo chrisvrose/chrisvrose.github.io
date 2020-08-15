ab64095042072351187e3753885d43fb022ff6a6
Atreya Bain <christophervrose@gmail.com>
2020-08-16 00:44:53 +0530

    Updates

---
+++ precache-manifest.d08052f0ed6106f3d0a0c3345f5c2c78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b20b9d089e0f77d7951378a719f147",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68461dd750939bab5351",
+    "url": "/static/css/main.21df61d6.chunk.css"
+  },
+  {
+    "revision": "47178b4c97a64c717815",
+    "url": "/static/js/2.dbc1bc78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dbc1bc78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68461dd750939bab5351",
+    "url": "/static/js/main.0bc80913.chunk.js"
+  },
+  {
+    "revision": "f52668fb72d1c7ab7506",
+    "url": "/static/js/runtime-main.3bf532aa.js"
+  }
+]);
