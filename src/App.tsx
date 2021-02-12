40a23c7a9678e5fd72a69fa63a9b2a53d82b7a72
Atreya Bain <christophervrose@gmail.com>
2021-02-12 22:11:38 +0530

    write pass 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
-import Content from './Content';
+import About from './About';
 
 export default function App() {
-  return (
-    <div className="App">
-      <Header text="Hi"></Header>
-      <Content />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header text="Hi"></Header>
+            <HashRouter>
+                <Route exact path="/" component={About} />
+            </HashRouter>
+            <Footer />
+        </div>
+    );
 }
--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import Social from './misc/SocialLink';
 import './Footer.css';
+import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import {
+    Restore as RestoreIcon,
+    AccessAlarm as FavoriteIcon,
+    LocationOn as LocationOnIcon,
+} from '@material-ui/icons';
 
-export default class Footer extends React.Component {
-    render() {
-        return (
-            <div className="para icon-box">
-                <Social name="Gitlab" url="https://gitlab.com/chrisvrose" picUrl="./static/projects/gitlab-white-small.svg" />
-                <Social name="Github" url="https://github.com/chrisvrose" picUrl="./static/projects/GitHub-Mark-Light-64px.png" />
-            </div>
-        );
-    }
+const onChangeVarState = (
+    state: string,
+    setState: React.Dispatch<React.SetStateAction<string>>
+) => (event: ChangeEvent<{}>, newState: string) => {
+    setState(newState);
+};
+
+export default function Footer() {
+    const [value, setValue] = React.useState('Recents');
+
+    return (
+        <BottomNavigation
+            onChange={onChangeVarState(value, setValue)}
+            value={value}
+        >
+            <BottomNavigationAction
+                label="Recents"
+                value="Recents"
+                icon={<RestoreIcon />}
+            />
+            <BottomNavigationAction
+                label="Favorites"
+                value="Fav"
+                icon={<FavoriteIcon />}
+            />
+            <BottomNavigationAction
+                label="Nearby"
+                value="Nearby"
+                icon={<LocationOnIcon />}
+            />
+        </BottomNavigation>
+    );
+    // return (
+    //     <div className="para icon-box">
+    //         <Social name="Gitlab" url="https://gitlab.com/chrisvrose" picUrl="./static/projects/gitlab-white-small.svg" />
+    //         <Social name="Github" url="https://github.com/chrisvrose" picUrl="./static/projects/GitHub-Mark-Light-64px.png" />
+    //     </div>
+    // );
 }
