d24276e143d83a9c09c0e4c3051587508a398214
Atreya Bain <christophervrose@gmail.com>
2021-03-11 19:11:14 +0530

    Add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/404.module.scss';
+
+export default function Page404() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.highlight}>404&nbsp;</div>| Not found
+        </div>
+    );
+}
