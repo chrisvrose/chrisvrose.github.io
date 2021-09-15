4b57d671aea56b72084fd72fb64b87f43233b3fe
Atreya Bain <christophervrose@gmail.com>
2021-09-15 19:45:27 +0530

    [add] 404 link

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,13 @@
 import styles from '../styles/404.module.scss';
+import Link from 'next/link';
 
 export default function Page404() {
     return (
         <div className={styles.container}>
-            <div className={styles.highlight}>404&nbsp;</div>| Not found
+            <div className={styles.highlight}>
+                <Link href='/'>404</Link>&nbsp;
+            </div>
+            | Not found
         </div>
     );
 }
