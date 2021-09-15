8a2594cfceaa17b90cb2e410b57299901343cf16
Atreya Bain <christophervrose@gmail.com>
2021-09-16 00:34:30 +0530

    [add] cname changes, trim public dir

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,6 +13,8 @@ const Header: FC<HeaderProps> = function Header({ title }) {
     return (
         <Head>
             <title>{title}</title>
+            <link rel='shortcut icon' href='https://secure.gravatar.com/avatar/1f56e31aa75edc98a8b9bc08431c8214?s=16' />
+            <meta name='description' content='Landing page for kekvrose.me' />
         </Head>
     );
 };
