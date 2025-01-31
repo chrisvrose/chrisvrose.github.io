2b99e4cc8a9e00af02c391e6b555712d1965ac19
Atreya Bain <atreyabain@gmail.com>
2025-01-31 19:09:36 +0000

    update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    publicExcludes: ['!static/**/*'],
+const withPWA = require("next-pwa")({
+  dest: "public",
+  publicExcludes: ["!static/**/*"],
 });
 
 /**
  * @type {import('next').NextConfig}
  */
 module.exports = withPWA({
-    // default config
-    output: 'export',
+  // default config
+  output: "export",
+  reactStrictMode: true,
 });
